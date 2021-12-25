b8c8b0ad63875b4948fa3d1ecb5c586aa1be96a4
Dylan Warrener <dryanwarrener@gmail.com>
2021-12-25 02:12:35 +0000

    Added new homepage files

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './home.scss';
+
+import Directory from '../../components/directory/directory';
+
+const HomePage = () => (
+    <div className='home-page'>
+        <Directory />
+    </div>
+);
+
+export default HomePage;
