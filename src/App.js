c51f7e47e3cc35275296a22efb8d36572b2af035
Dylan Warrener <dryanwarrener@gmail.com>
2021-12-25 02:10:35 +0000

    Added routes in App.js + styles for an a tag

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
-
-import HomePage from './pages/homepage/homepage.jsx';
+import { Routes, Route } from 'react-router-dom';
 
 import './App.css';
 
-function App() {
+import HomePage from './pages/home/home';
+import ShopPage from './pages/shop/shop';
+import ContactPage from './pages/contact/contact';
+import Header from './components/header/header';
+
+export default function App() {
   return (
-    <div>
-      <HomePage />
+    <div className='App'>
+      <Header />
+      <Routes>
+        <Route exact path='/contact' element={<ContactPage />} />
+        <Route exact path='/shop' element={<ShopPage />} />
+        <Route exact path='/' element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
-
-export default App;
