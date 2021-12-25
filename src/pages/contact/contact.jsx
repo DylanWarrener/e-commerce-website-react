53bc84380829fcc49b844f788f215cd3bbbe29a6
Dylan Warrener <dryanwarrener@gmail.com>
2021-12-25 02:09:02 +0000

    Added a contact page

---
+++ src/pages/contact/contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './contact.scss';
+
+export default function ContactPage() {
+    return (
+        <div className="contact-page">
+            Contact page
+		</div>
+    );
+}
