dfd1645db589005c9c05419cc52338a108bede3f
Dylan Warrener <dryanwarrener@gmail.com>
2021-12-29 10:11:57 +0000

    Added the logo in header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,10 +3,14 @@ import { Link } from 'react-router-dom';
 
 import './header.scss';
 
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
 export default function Header() {
     return (
         <div className="header">
-			<Link className="logo-container" to="/"></Link>
+			<Link className="logo-container" to="/">
+				<Logo className='logo' />
+			</Link>
 			<div className="nav-menu">
 				<Link className="options" to="/shop">
 					SHOP
