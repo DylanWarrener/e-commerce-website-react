ed783c2bcc67f55d0f0d603167ae0087439f2e68
Dylan Warrener <dryanwarrener@gmail.com>
2021-12-29 10:10:16 +0000

    Added contact and shop page to routes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 
 import HomePage from './pages/home/home';
 import ShopPage from './pages/shop/shop';
-import ContactPage from './pages/contact/contact';
+import ContactPage from './pages/sign-in-sign-up/sign-in-sign-up';
+import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up';
 import Header from './components/header/header';
 
 export default function App() {
@@ -13,6 +14,7 @@ export default function App() {
     <div className='App'>
       <Header />
       <Routes>
+        <Route exact path='/signin' element={<SignInSignUpPage />} />
         <Route exact path='/contact' element={<ContactPage />} />
         <Route exact path='/shop' element={<ShopPage />} />
         <Route exact path='/' element={<HomePage />} />
