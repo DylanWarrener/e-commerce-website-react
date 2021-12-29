fae5f46203d2e0fc9ce435e9d804fbaeca2081c9
Dylan Warrener <dryanwarrener@gmail.com>
2021-12-29 10:12:41 +0000

    Tidied the collection-preview component

--- src/components/collection/collection-preview.jsx
+++ src/components/collection/collection-preview.jsx
@@ -7,11 +7,12 @@ export default function CollectionPreview(props) {
         <div className="collection-preview">
 			<h1 className="title">{props.title.toUpperCase()}</h1>
 			<div className="preview">
-				{props.items
-					.filter((item, index) => index < 4)
+				{
+					props.items.filter((item, index) => index < 4)
 					.map(({ id, ...otherItemProps }) => (
 						<CollectionItem key={id} {...otherItemProps} />
-					))}
+					))
+				}
 			</div>
 		</div>
     );
