f9481fb01c56d21a28b3f498a4b4ead288f552a4
Dylan Warrener <dryanwarrener@gmail.com>
2022-01-04 09:32:31 +0000

    Added comments to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+// React 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+// React router 
 import { BrowserRouter } from 'react-router-dom';
 
+// Main app
+import App from './App';
+
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Style sheets
 import './index.css';
 
 ReactDOM.render(
