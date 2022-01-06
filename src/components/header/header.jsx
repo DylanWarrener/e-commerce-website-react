fca7d85ea8b7efbc708b5b71fc973072f68448a8
Dylan Warrener <dryanwarrener@gmail.com>
2022-01-06 16:43:17 +0000

    Added a sign in and sign out button/Link

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { auth } from '../../firebase/firebase.util';
 
 import './header.scss';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-export default function Header() {
+export default function Header({ currentUser }) {
     return (
         <div className="header">
 			<Link className="logo-container" to="/">
@@ -18,6 +19,11 @@ export default function Header() {
 				<Link className="options" to="/contact">
 					CONTACT
 				</Link>
+				{
+					currentUser 
+					? <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+					: <Link className='option' to='/signin'>SIGN IN</Link>
+				}
 			</div>
 		</div>
     );
