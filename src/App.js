ff2c5fd2952f14f4b0065bbce16e482854ee8950
Dylan Warrener <dryanwarrener@gmail.com>
2022-01-06 16:42:26 +0000

    Changed app to class component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { auth } from './firebase/firebase.util';
 
 import './App.css';
 
@@ -9,16 +10,40 @@ import ContactPage from './pages/sign-in-sign-up/sign-in-sign-up';
 import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up';
 import Header from './components/header/header';
 
-export default function App() {
-  return (
-    <div className='App'>
-      <Header />
-      <Routes>
-        <Route exact path='/signin' element={<SignInSignUpPage />} />
-        <Route exact path='/contact' element={<ContactPage />} />
-        <Route exact path='/shop' element={<ShopPage />} />
-        <Route exact path='/' element={<HomePage />} />
-      </Routes>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth= auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+  
+  render () {
+    return (
+      <div className='App'>
+        <Header currentUser={this.state.currentUser}/>
+        <Routes>
+          <Route exact path='/signin' element={<SignInSignUpPage />} />
+          <Route exact path='/contact' element={<ContactPage />} />
+          <Route exact path='/shop' element={<ShopPage />} />
+          <Route exact path='/' element={<HomePage />} />
+        </Routes>
+      </div>
+    );
+  }
 }
+
+export default App;
