446b717f17503d0b22decc0245ce44651ccd8a9b
Dylan Warrener <dryanwarrener@gmail.com>
2022-01-10 16:24:24 +0000

    Nothing changed

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -35,7 +35,7 @@ class SignIn extends React.Component {
                 <h2 className="title">I already have an account</h2>
                 <span>Sign in with your email and password</span>
 
-                <form onSubmit={this.handleSubmit}>
+                <form className='sign-in-form' onSubmit={this.handleSubmit}>
                     <FormInput 
                         name='email' 
                         type='email' 
@@ -52,11 +52,13 @@ class SignIn extends React.Component {
                         label='Password'
                         required 
                     />
+                    <div className="buttons">
                     <CustomButton type='submit'>Sign in</CustomButton>
                     <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
                         {' '}
                         Sign in with Google {' '}
                     </CustomButton>
+                    </div>
                 </form>
             </div>
         );
