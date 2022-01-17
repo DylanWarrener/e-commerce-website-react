b7b99a4297c614ab3efb12b3624a4f2a8bf8ee85
Dylan Warrener <dryanwarrener@gmail.com>
2022-01-17 07:26:53 +0000

    Default structure for file

---
+++ src/components/contact/contact.jsx
@@ -0,0 +1,13 @@
+// Default react
+import React from 'react';
+
+// Stylesheet
+import './contact.scss';
+
+export default function contact(props) {
+    return (
+        <div>
+            Contact 
+        </div>
+    );
+}
