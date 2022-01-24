04a355e60c190834d10827290c8c1078c4b66fd3
Dylan Warrener <dryanwarrener@gmail.com>
2022-01-24 11:48:16 +0000

    Imported the user action types

--- src/redux/user/user-action.js
+++ src/redux/user/user-action.js
@@ -1,5 +1,7 @@
+import { UserActionTypes } from "./user-types";
+
 // Set current user to the type and payload for dispatch
 export const setCurrentUser = user => ({
-    type: 'SET_CURRENT_USER',
+    type: UserActionTypes.SET_CURRENT_USER,
     payload: user
 });
