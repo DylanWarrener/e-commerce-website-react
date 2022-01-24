a99f947df9ab938066d52ebe072ef56a4885ba91
Dylan Warrener <dryanwarrener@gmail.com>
2022-01-24 09:58:37 +0000

    Added a functional component to contact

---
+++ src/pages/contact/contact.jsx
@@ -0,0 +1,16 @@
+// Default react
+import React from 'react';
+
+// Components 
+import Contact from '../../components/contact/contact';
+
+// Stylesheet
+import './contact.scss';
+
+export default function ContactPage(props) {
+    return (
+        <div>
+            <Contact />
+        </div>
+    );
+}
