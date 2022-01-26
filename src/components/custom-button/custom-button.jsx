dbde9a16c3a02a8377a4727ec865a98b587c8c32
Dylan Warrener <dryanwarrener@gmail.com>
2022-01-26 12:07:50 +0000

    Added inverted prop to change class name of button

--- src/components/custom-button/custom-button.jsx
+++ src/components/custom-button/custom-button.jsx
@@ -4,11 +4,14 @@ import React from 'react';
 // Stylesheet
 import './custom-button.scss';
 
-export default function CustomButton({ children, isGoogleSignIn, ...otherProps }) {
+export default function CustomButton({ children, isGoogleSignIn, inverted, ...otherProps }) {
     return (
         <button 
             type='button'
-            className={`${isGoogleSignIn ? 'google-sign-in': '' } custom-button`}
+            className={`
+                ${inverted ? 'inverted': '' } 
+                ${isGoogleSignIn ? 'google-sign-in': '' } custom-button
+            `}
             {...otherProps}
         >
             {children}
