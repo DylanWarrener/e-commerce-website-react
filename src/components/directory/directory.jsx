f42d788fbc5acc07132253432eba057e223af603
Dylan Warrener <dryanwarrener@gmail.com>
2022-01-26 13:54:44 +0000

    Changed class name

--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -12,7 +12,7 @@ import './directory.scss';
 
 export default function Directory() {
     return (
-        <div className='directory-menu'>
+        <div className='directory-container'>
             {SHOP_DATA.map(({ id, title, imageUrl, size }) => (
                 <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
             ))}
