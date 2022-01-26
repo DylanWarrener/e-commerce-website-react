d5f7cf3c1660b0662eee041042a54674d73a1ae6
Dylan Warrener <dryanwarrener@gmail.com>
2022-01-26 11:49:24 +0000

    imported redux & action. Used redux to toggle cart

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -1,17 +1,25 @@
 // Default react
 import React from 'react';
 
+// Redux
+import { connect } from 'react-redux';
+import { toggleCartHidden } from '../../redux/cart/cart-action';
+
 // Components
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 // Stylesheet
 import './cart-icon.scss';
 
-export default function CartIcon() {
-    return (
-        <div className='cart-container'>
-            <ShoppingIcon className='shopping-icon' />
-            <span className='item-count'>0</span>
-        </div>
-    );
-}
+const CartIcon = ({ toggleCartHidden }) => (
+    <div className='cart-container' onClick={toggleCartHidden}>
+        <ShoppingIcon className='shopping-icon' />
+        <span className='item-count'>0</span>
+    </div>
+);
+
+const mapDispatchToProps = dispatch => ({
+    toggleCartHidden: () => dispatch(toggleCartHidden())
+})
+
+export default connect(null, mapDispatchToProps)(CartIcon);
