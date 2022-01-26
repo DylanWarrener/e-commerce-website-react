d2d21463dafde53390b46aa8cb421e3e97ccd29a
Dylan Warrener <dryanwarrener@gmail.com>
2022-01-26 13:54:06 +0000

    Destructured props

--- src/components/collection/collection-item.jsx
+++ src/components/collection/collection-item.jsx
@@ -7,20 +7,20 @@ import CustomButton from '../custom-button/custom-button';
 // Stylesheet
 import './collection-item.scss';
 
-export default function CollectionItem(props) {
+export default function CollectionItem({ name, price, imageUrl }) {
     return (
         <div className="collection-item">
 			<div
 				className="image"
 				style={{
-					backgroundImage: `url(${props.imageUrl})`
+					backgroundImage: `url(${imageUrl})`
 				}}
 			/>
-			<div className="collection-footer">
-				<span className="name">{props.name}</span>
-				<span className="price">£{props.price}</span>
-			</div>
 			<CustomButton inverted>Add to cart</CustomButton>
+			<div className="collection-footer">
+				<span className="name">{name}</span>
+				<span className="price">£{price}</span>
+			</div>
 		</div>
     );
 }
