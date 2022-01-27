73b5f16858a186db1bb7b80ac65ee778eb4b892d
Dylan Warrener <dryanwarrener@gmail.com>
2022-01-27 11:59:07 +0000

    Added the base elements for a functional component

---
+++ src/pages/checkout/checkout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './checkout.scss';
+
+const Checkout = () => {
+    return (
+        <div>
+            Checkout page
+        </div>
+    )
+};
+
+export default Checkout;
