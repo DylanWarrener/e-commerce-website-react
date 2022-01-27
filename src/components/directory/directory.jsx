5a1803b513cd05d290e56d087474349c30599e45
Dylan Warrener <dryanwarrener@gmail.com>
2022-01-27 11:32:34 +0000

    Reformated

--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -13,9 +13,11 @@ import './directory.scss';
 export default function Directory() {
     return (
         <div className='directory-container'>
-            {SHOP_DATA.map(({ id, title, imageUrl, size }) => (
-                <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
-            ))}
+            {
+                SHOP_DATA.map(({ id, title, imageUrl, size }) => (
+                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                ))
+            }
         </div>
     );
 }
