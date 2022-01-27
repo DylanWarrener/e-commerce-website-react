c365f52366f6c80615c46b60abb2f44b4321f12c
Dylan Warrener <dryanwarrener@gmail.com>
2022-01-27 09:39:47 +0000

    Map through the items array

--- src/components/collection/collection-preview.jsx
+++ src/components/collection/collection-preview.jsx
@@ -14,8 +14,8 @@ export default function CollectionPreview({ title, items }) {
 			<div id='collection-item'>
 				{
 					items.filter((item, index) => index < 4)
-					.map(({ id, ...otherItemProps }) => (
-						<CollectionItem key={id} {...otherItemProps} />
+					.map((item) => (
+						<CollectionItem key={item.id} item={item} />
 					))
 				}
 			</div>
