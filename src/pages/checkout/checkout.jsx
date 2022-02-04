0f9525cddd1bfc52f7716216398885e29a11b495
Dylan Warrener <dryanwarrener@gmail.com>
2022-02-04 12:01:04 +0000

    Added checkout content

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 
+// Stylesheet
 import './checkout.scss';
 
 const Checkout = () => {
     return (
-        <div>
-            Checkout page
+        <div className='checkout-page'>
+            <div className='checkout-header'>
+                <div className='header-block'>
+                    <span>Product</span>
+                </div>
+                <div className='header-block'>
+                    <span>Description</span>
+                </div>
+                <div className='header-block'>
+                    <span>Quantity</span>
+                </div>
+                <div className='header-block'>
+                    <span>Price</span> 
+                </div>
+                <div className='header-block'>
+                    <span>Remove</span>
+                </div>
+            </div>
         </div>
     )
 };
-
+                
 export default Checkout;
